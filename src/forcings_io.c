4defae6e7176b7dd40dfd1e18f63895f9285d797
Samuel Debionne <samuel-debionne@uiowa.edu>
2017-03-27 08:50:18 -0500

    Re-add date_manip files

--- src/forcings_io.c
+++ src/forcings_io.c
@@ -25,6 +25,7 @@ extern int np;
 
 #include <compression.h>
 #include <comm.h>
+#include <date_manip.h>
 #include <db.h>
 #include <sort.h>
 #include <forcings_io.h>
@@ -1108,30 +1109,6 @@ int Create_Rain_Database_Irregular(
     return 0;
 
 #endif //HAVE_POSTGRESQL
-}
-
-
-static int days_in_month(int mon, int year)
-{
-    const int days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    //int mon = current_time->tm_mon - 1;
-
-    if (mon == 1)	//Damn you February...
-    {
-        //int year = current_time->tm_year;
-
-        if (year % 4)	return days[mon];	//Not leap year
-        if (year % 100)	return days[mon] + 1;	//Leap year
-        if (year % 400)	return days[mon];	//Not leap year
-        return days[mon] + 1;			//If this line is reached, then the current year is at least 2400.
-                                        //I cannot explain to you in words how terrified I am to think that someone has
-                                        //used code I wrote for 400 years. Seriously, something better and with
-                                        //less bugs has surely come along. Also, anyone reading this in the year 2400 is
-                                        //probably a robot, which is pretty cool.
-                                        //--Scott,  April 2, 2014
-    }
-    else
-        return days[mon];
 }
 
 
